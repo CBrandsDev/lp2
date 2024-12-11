83bb8c61579198d565de45e3cbec9eb143f4a820
CBrandsDev <cbrandao2001@gmail.com>
2024-12-11 11:57:18 -0300

    finished

--- Prova_2_unidade/CadernetaPoupancaUtil.java
+++ Prova_2_unidade/CadernetaPoupancaUtil.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class CadernetaPoupancaUtil {
     public static void main(String[] args) {
+        clear();
         Scanner sc = new Scanner(System.in);
         ArrayList<CadernetaPoupanca> caderneta = new ArrayList<>();
+        CadernetaPoupanca cadernetaEncontrada = null;
         int quantidade = 7;
         int index = 1;
         int opcao;
@@ -27,7 +29,11 @@ public class CadernetaPoupancaUtil {
                         System.out.println("Qual o dia do aniversário da conta?");
                         int diaAniversario = sc.nextInt();
                         sc.nextLine();
-
+                        while (diaAniversario < 1 || diaAniversario > 31) {
+                            System.out.println("Digite um dia válido (1 - 31):");
+                            diaAniversario = sc.nextInt();
+                            sc.nextLine();
+                        }
                         System.out.println("Qual o saldo inicial da conta?");
                         double saldoInicial = sc.nextDouble();
                         sc.nextLine();
@@ -39,16 +45,96 @@ public class CadernetaPoupancaUtil {
                     break;
 
                 case 2:
+                    clear();
+                    System.out.println("Digite o dia do aniversário que deseja consultar:");
+                    int consultaDia = sc.nextInt();
+                    sc.nextLine();
+
+                    boolean encontrado = false;
+                    for (CadernetaPoupanca cp : caderneta) {
+                        if (cp.getDiaAniversario() == consultaDia) {
+                            clear();
+                            System.out.println("|Caderneta encontrada:");
+                            System.out.println("|Titular: " + cp.getTitular());
+                            System.out.println("|Dia de Aniversário: " + cp.getDiaAniversario());
+                            System.out.println("|Saldo: " + cp.getSaldo());
+                            System.out.println("|-----------------------------------------------------|");
+
+                            encontrado = true;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("Nenhuma caderneta encontrada com o dia de aniversário informado.");
+                    }
+
                     break;
                 case 3:
+                    clear();
+                    System.out.println("Digite o titular que deseja consultar:");
+                    String consultaTitular = sc.nextLine();
+
+                    encontrado = false;
+                    for (CadernetaPoupanca cp : caderneta) {
+                        if (cp.getTitular().equalsIgnoreCase(consultaTitular)) {
+                            clear();
+                            System.out.println("|Caderneta encontrada");
+                            System.out.println("|Titular: " + cp.getTitular());
+                            System.out.println("|Dia de Aniversário: " + cp.getDiaAniversario());
+                            System.out.println("|Saldo: " + cp.getSaldo());
+                            System.out.println("|-----------------------------------------------------|");
+                            encontrado = true;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("Nenhuma caderneta encontrada com o titular informado.");
+                    }
                     break;
                 case 4:
+                    clear();
+                    System.out.println("Digite o nome do titular da caderneta que deseja atualizar o rendimento: ");
+                    String titular = sc.next();
+                    for (CadernetaPoupanca cp : caderneta) {
+                        if (cp.getTitular().equalsIgnoreCase(titular)) {
+                            cadernetaEncontrada = cp;
+                            break;
+                        }
+                        cadernetaEncontrada = null;
+                    }
+                    if (cadernetaEncontrada == null) {
+                        System.out.println("Caderneta não encontrada!");
+                        return;
+                    }
+                    clear();
+                    System.out.println("|Rendimento atual: " + cadernetaEncontrada.getRendimentoAcumulado());
+                    System.out.println("Digite a taxa de rendimento: ");
+                    double taxaRendimento = sc.nextDouble();
+                    cadernetaEncontrada.atualizarRendimento(taxaRendimento);
+                    System.out.println("|Novo rendimento: " + cadernetaEncontrada.getRendimentoAcumulado());
+                    System.out.println("|Rendimento atualizado com sucesso!");
+                    System.out.println("|-----------------------------------------------------|");
                     break;
                 case 5:
+                    boot = false;
+                    System.out.println("Encerrando o programa...");
+                    HashSet<Integer> diasAniversario = new HashSet<Integer>();
+                    ArrayDeque<String> titulares = new ArrayDeque<>();
+                    for (CadernetaPoupanca cp : caderneta) {
+                        if (cp.getTaxaRendimentoAcumulado() != 0) {
+                            diasAniversario.add(cp.getDiaAniversario());
+                        } else {
+                            titulares.add(cp.getTitular());
+                        }
+                    }
+                    System.out.println("Quantidade total de dias de aniversário com rendimentos acumulados: " + diasAniversario.size());
+                    System.out.println("Titulares com rendimento acumulado igual a zero: ");
+                    do {
+                        System.out.println(titulares.poll());
+                    } while (!titulares.isEmpty());
                     break;
-
                 default:
-                    System.out.println("Opção inválida, tente novamente.");
+                    System.out.println("Opção inválida! Tente novamente.");
                     break;
             }
         }
