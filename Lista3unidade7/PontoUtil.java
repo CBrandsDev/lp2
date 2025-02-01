f0be9100a26e463f9ed215d80b2633cbb8fb00fd
CBrandsDev <cbrandao2001@gmail.com>
2025-02-01 12:46:22 -0300

    initial

---
+++ Lista3unidade7/PontoUtil.java
@@ -0,0 +1,10 @@
+package Lista3unidade7;
+import Lista3unidade7.Ponto;
+
+public class PontoUtil {
+   public static void main(String[] args) {
+        Ponto[] pontos = new Ponto[5];
+        
+
+   } 
+}
